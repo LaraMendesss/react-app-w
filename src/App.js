43163ffb37803d9e5b6f321f46877747f796e023
LaraMendesss <laraicmendes@gmail.com>
2024-01-12 16:28:05 -0500

    bootstrap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-   
+    <div className="App">
      
         <p>
           My Weather App - Lara Mendes
         </p>
         
      <footer>This project was coded by <a href="https://www.linkedin.com/in/lara-chagas-mendes/" target="_blank"rel="noreferrer">Lara Mendes</a> and it's open sourced on <a href="https://github.com/LaraMendesss/react-app-w" target="_blank"rel="noreferrer">GitHub</a> and hosted on <a href="https://react-lara-wapp.netlify.app/" target="_blank"rel="noreferrer">Netlify</a>.</footer>
-  </div>);
+     </div>
+     </div>);
 }
 
