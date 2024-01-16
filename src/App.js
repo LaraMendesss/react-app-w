df37d7abc628cfcb6c83a7a25942813653132d2d
LaraMendesss <laraicmendes@gmail.com>
2024-01-16 16:56:38 -0500

    Import Axios and call API

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
         <p>
           My Weather App - Lara Mendes
         </p>
-        <Weather />
+        <Weather city="New York"/>
      <footer>This project was coded by <a href="https://www.linkedin.com/in/lara-chagas-mendes/" target="_blank"rel="noreferrer">Lara Mendes</a> and it's open sourced on <a href="https://github.com/LaraMendesss/react-app-w" target="_blank"rel="noreferrer">GitHub</a> and hosted on <a href="https://react-lara-wapp.netlify.app/" target="_blank"rel="noreferrer">Netlify</a>.</footer>
      </div>
      </div>);
--- src/Weather.js
+++ src/Weather.js
@@ -1,41 +1,74 @@
-import React from "react";
+import React,{ useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 
-export default function Weather() {
+export default function Weather(props){ 
 
-    return(
-<div className="Weather">
-<form className="form" >
-    <div className="row">
-        <div className="col-6">
-<input className="form-control"type="search"placeholder="Enter a city"/></div>
-<div className="col-3">
-<input className="btn btn-primary" type="submit" value="Search" /></div>
-</div>
-    </form>
-<h1>New York</h1>
-<ul>
-    <li>Wednesday 07:00</li>
-    <li>Mostly Cloudy</li>
-</ul>
-<div className="row">
-    <div className="col-6">
+const [data, setData]=useState({ ready: false });
 
-        <img src="https://user-images.githubusercontent.com/139086341/280107452-9eff79b3-5881-43ec-b5a0-2f62080d8bb2.jpg" width="100" alt="weather icon"/>
+function displayData(response){
+    console.log(response.data.main.temp);
+  
+    setData({
+        ready: true,
+        temperature: Math.round(response.data.main.temp),
+        humidity: response.data.main.humidity,
+        wind: response.data.wind.speed,
+        city: response.data.name,
+        icon: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+        description: response.data.weather[0].description,
+        date: "Wednesday 07:00",
+    });
+    }
 
-    
-        6°C
+
+if (data.ready)
+{
+return(
+    <div className="Weather">
+    <form className="form" >
+        <div className="row">
+            <div className="col-6">
+    <input className="form-control"type="search"placeholder="Enter a city" autoFocus="on"/></div>
+    <div className="col-3">
+    <input className="btn btn-primary w-100" type="submit" value="Search" /></div>
     </div>
-    <div className="col-6">
-    
+        </form>
+    <h1 className="text-capitalize">{data.city}</h1>
     <ul>
-    <li>Precipition: 15%</li>
-    <li>Humidity: 72%</li>
-    <li>Wind: 13km/h</li>
-</ul>
-</div>
-
+        <li>{data.date}</li>
+        <li className="text-capitalize">{data.description}</li>
+    </ul>
+    <div className="row mt-3">
+        <div className="col-6 details">
+    
+            <img src={data.icon} alt={data.description} width="100"  className="float-left"/>
+    
+        <div className="tempUnit">
+          <span className="temperature">{data.temperature}</span>
+          <span className="unit">°C</span>
+          </div>
+        </div>
+        <div className="col-6">
+        
+        <ul>
+       <br/>
+        <li>Humidity: {data.humidity}%</li>
+        <li>Wind: {data.wind}km/h</li>
+    </ul>
     </div>
-</div>)
-}
+    
+        </div>
+    </div>);}
+
+    else{
+
+let apiKey=`62231151ce343c4d68652e1617efc22f`;
+
+let apiUrl=`http://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`
+axios.get(apiUrl).then(displayData);
+
+return "Loading";
+
+}}
